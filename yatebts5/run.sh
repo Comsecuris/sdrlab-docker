742310b9f4c305cc8758467f285cd574958b0faf
User <ralf@comsecuris.com>
2018-02-07 05:25:29 -0800

    expose YateBTS' rmanager port to host (on localhost only).

--- yatebts5/run.sh
+++ yatebts5/run.sh
@@ -2,4 +2,4 @@
 
 NAME=`basename $PWD`
 
-docker run -t -i -h yatebts5 --privileged -v /dev/bus/usb:/dev/bus/usb -h $NAME $NAME
+docker run -p 127.0.0.1:5038:5038 -t -i -h yatebts5 --privileged -v /dev/bus/usb:/dev/bus/usb $NAME bash
